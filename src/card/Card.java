8866fa1fea3729b07411269882a9594595e2780d
rav130 <kau14941@sheridancollege.ca>
2023-05-28 23:00:14 -0400

    student details version 2

--- src/card/Card.java
+++ src/card/Card.java
@@ -47,6 +47,7 @@ public class Card {
         this.value = value;
     }
    
-   
+ 
+  
     
 }
--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,8 +1,15 @@
-/*
+*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+//Name: Ravneet Kaur
+//Student ID: 991708968
+
+
 package card;
+
+import java.util.*;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -15,16 +22,42 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Scanner scan =new Scanner(System.in);
+        
+        
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            
+            c.setValue((int)(Math.random()*13+1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*3+0)]);
+            
+            magicHand[i]=c;
+            System.out.println("Random card"+magicHand[i].getSuit()+""+magicHand[i].getValue());
         }
         
+        
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Enter the value of Choice");
+        int userValue =scan.nextInt();
+        
+        System.out.println("Enter the suit of Choice");
+        String userSuit=scan.next();
+        
+        
+       
         // and search magicHand here
+        for (int i=0; i<magicHand.length; i++){
+            if(userValue==magicHand[i].getValue() ||userSuit.equals(magicHand[i].getSuit())){
+                System.out.println("Card Matches");
+            }
+            else {
+                System.out.println("Card does not match");
+            }
+        }
+        
         //Then report the result here
     }
     
